be2afdb6378245c454cdfab8bfb6a9092dc7919c
Cody Lee <codylee@tactica.ca>
2016-04-21 16:20:13 -0500

    Installing docker with -y flag.

--- scripts/centos/install-docker.sh
+++ scripts/centos/install-docker.sh
@@ -14,7 +14,7 @@ if [ ! "$haveDocker" ]; then
   sudo curl -O -sSL http://get.docker.com/docker/1.7.0/rpms/centos-6/RPMS/x86_64/docker-engine-1.7.0-0.1.el6.x86_64.rpm
 
   #install docker
-  sudo yum localinstall --nogpgcheck docker-engine-1.7.0-0.1.el6.x86_64.rpm
+  sudo yum -r localinstall --nogpgcheck docker-engine-1.7.0-0.1.el6.x86_64.rpm
 
   #start docker
   sudo service docker start
