480212d28642d3f2c9926e24470831188b34c0f3
Cody Lee <codylee@tactica.ca>
2016-04-21 16:41:52 -0500

    Add -y flag for yum update also.

--- scripts/centos/install-docker.sh
+++ scripts/centos/install-docker.sh
@@ -8,13 +8,13 @@ set -e
 if [ ! "$haveDocker" ]; then
 
   # Required to update system
-  sudo yum update
+  sudo yum -y update
 
   # Download docker rpm
   sudo curl -O -sSL http://get.docker.com/docker/1.7.0/rpms/centos-6/RPMS/x86_64/docker-engine-1.7.0-0.1.el6.x86_64.rpm
 
   #install docker
-  sudo yum -r localinstall --nogpgcheck docker-engine-1.7.0-0.1.el6.x86_64.rpm
+  sudo yum -y localinstall --nogpgcheck docker-engine-1.7.0-0.1.el6.x86_64.rpm
 
   #start docker
   sudo service docker start
